f648924f5884002165807a4d98372dde08fbcd1f
Francisco <franciscoelder0@gmail.com>
2023-03-30 19:13:25 -0300

    final

--- src/components/contacto/index.jsx
+++ src/components/contacto/index.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import styles from './contacto.module.scss';
+import { useState } from 'react';
 
 const Contacto = () => {
+  const [email, setEmail] = useState('');
+  const [asunto, setAsunto] = useState('');
+  const [mensaje, setMensaje] = useState('');
   return (
     <div className={styles.container}>
       <h2>Contacto</h2>
+      <form>
+        <label htmlFor='email'>Email</label>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor='asunto'>Asunto</label>
+        <input
+          type='text'
+          id='asunto'
+          name='asunto'
+          value={asunto}
+          onChange={(e) => setAsunto(e.target.value)}
+        />
+        <label htmlFor='mensaje'>Mensaje</label>
+        <textarea
+          id='mensaje'
+          name='mensaje'
+          value={mensaje}
+          onChange={(e) => setMensaje(e.target.value)}
+        />
+        <button type='submit'>Enviar</button>
+      </form>
     </div>
   );
 };
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Styles from './footer.module.scss';
 
 const Footer = () => {
-  return <div className={Styles.footer}>footer</div>;
+  return (
+    <div className={Styles.footer}>
+      <h6>Pagina creada por fan y sin fines de lucro</h6>
+      <a href='https://www.pokemon.com/es'>Sitio oficial de pokemon</a>
+    </div>
+  );
 };
 
 export default Footer;
